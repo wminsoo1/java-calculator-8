4993dd94f6fb70d9f438c1214c7385cce4b1d72a
wjunseok <wminsoo1@naver.com>
2025-10-19 16:48:48 +0900

    chore: Application 클래스에 메인 로직 통합 및 프로그램 실행 완성
    - InputView, StringCalculator, OutputView를 Application.main() 메서드에서 연결
    - 사용자 입력부터 계산, 결과 출력까지의 프로그램 전체 흐름 완성 및 실행 가능하도록 구성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
 
+import static calculator.io.InputView.readInput;
+import static calculator.io.OutputView.printResult;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = readInput();
+
+        StringCalculator calculator = new StringCalculator(new DelimiterParser(), new InputAnalyzer(input));
+
+        int result = calculator.calculate();
+
+        printResult(result);
+
     }
 }
