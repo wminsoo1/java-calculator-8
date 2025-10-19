c3f52c6653fb41c503c909397fcacf668ee63fc7
wjunseok <wminsoo1@naver.com>
2025-10-19 22:19:22 +0900

    feat(parser): DefaultDelimiterParser 클래스 구현
    - DelimiterParser 인터페이스를 구현하여 기본 구분자(','와 ':')로 문자열을 분리하는 파서 로직 구현

---
+++ src/main/java/calculator/parser/DefaultDelimiterParser.java
@@ -0,0 +1,14 @@
+package calculator.parser;
+
+import java.util.List;
+
+public class DefaultDelimiterParser implements DelimiterParser {
+
+    private static final String DEFAULT_DELIMITERS = ",|:";
+
+    @Override
+    public List<String> parser(String input) {
+        return List.of(input.split(DEFAULT_DELIMITERS));
+    }
+
+}
